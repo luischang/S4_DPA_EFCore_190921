ed5e8e99fb9b262d699738879ab5f08a1019a7a5
Luis Chang <luis.chang@outlook.com>
2021-09-26 13:50:09 -0500

    MVC EF Core Scaffolding

---
+++ S5_DPA_MVCEFCORE.DatabaseFirst/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace S5_DPA_MVCEFCORE.DatabaseFirst.Models
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            Order = new HashSet<Order>();
+        }
+
+        public int Id { get; set; }
+        [Display(Name="Nombre")]
+        public string FirstName { get; set; }
+        [Display(Name = "Apellido")]
+        public string LastName { get; set; }
+        [Display(Name = "Ciudad")]
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+
+        public virtual ICollection<Order> Order { get; set; }
+    }
+}
---
+++ S5_DPA_MVCEFCORE.DatabaseFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace S5_DPA_MVCEFCORE.DatabaseFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
